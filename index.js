2e2e0e2d1b361ef46a3d8d0e8aa170aceb2979de
IidaPeltonen <n0peii00@students.oamk.fi>
2022-04-22 12:41:17 +0300

    uusi build

--- index.js
+++ index.js
@@ -133,8 +133,6 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
